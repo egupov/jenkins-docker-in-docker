50593d8b3c7d9c615be875972f0821bb5276d724
Andrey Egupov <egupov4727@gmail.com>
2022-10-19 16:42:11 +0300

    test

---
+++ Image/build-start.sh
@@ -0,0 +1,2 @@
+docker image build -t maven-agent . && docker tag maven-agent:latest egupoff/alpine-maven-agent:latest && docker push egupoff/alpine-maven-agent:latest
+#&& docker run -it -v "/var/run/docker.sock:/var/run/docker.sock:rw" maven-agent /bin/sh
